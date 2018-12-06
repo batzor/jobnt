bd0c485f46925abd718f3fed1cbe6c5a4177565e
anuartb <anuartb@gmail.com>
2018-12-06 21:14:53 +0900

    Changed catalog item list to dynamic
    Also:
    + Setted up the search routine
    
    TODO:
    - Implement view as class
    - Enable filtering

--- jobnt/catalog/views.py
+++ jobnt/catalog/views.py
@@ -1,12 +1,21 @@
 from django.shortcuts import render
 from .forms import JobSearchForm
+from .models import JobOffer
 
 # Create your views here.
 def index(request):
   form = JobSearchForm(request.GET)
   if form.is_valid():
-    name = form.cleaned_data['name']
-    #DEBUG
-    #print(name)
-
+    name = form.cleaned_data['name_field']
+    deadline = form.cleaned_data['date_field']
+    location = form.cleaned_data['location_field']
+    offers = JobOffer.objects.all() 
+    if name:
+      pass
+    if deadline:
+      pass
+    if location:
+      pass
+    args = {'form': form, 'offers': offers}
+    return render(request, 'catalog/index.html', args)
   return render(request, 'catalog/index.html', {'form': form})
