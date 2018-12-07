524ea93f0309286ac99e7a9a217c3101bdd58d52
anuartb <anuartb@gmail.com>
2018-12-07 22:46:32 +0900

    Enhanced filter frontend

--- jobnt/catalog/views.py
+++ jobnt/catalog/views.py
@@ -4,18 +4,7 @@ from .models import JobOffer
 
 # Create your views here.
 def index(request):
-  form = JobSearchForm(request.GET)
-  if form.is_valid():
-    name = form.cleaned_data['name_field']
-    deadline = form.cleaned_data['date_field']
-    location = form.cleaned_data['location_field']
-    offers = JobOffer.objects.all() 
-    if name:
-      pass
-    if deadline:
-      pass
-    if location:
-      pass
-    args = {'form': form, 'offers': offers}
-    return render(request, 'catalog/index.html', args)
-  return render(request, 'catalog/index.html', {'form': form})
+  form = JobSearchForm()
+  offers = JobOffer.objects.all()
+  args = {'form': form, 'offers': offers}
+  return render(request, 'catalog/index.html', args)
