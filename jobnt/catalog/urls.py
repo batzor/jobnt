3a3178ecae269ed871af95b49220da8cae8f5b40
qvntm <batzorig@kaist.ac.kr>
2018-12-09 19:26:40 +0900

    Implemented favorites page

--- jobnt/catalog/urls.py
+++ jobnt/catalog/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
 from . import subs
+from . import favs
 
 urlpatterns = [
   path('', views.index, name='index'),
   path('search/', views.search, name='search'),
   path('subs/', subs.index, name='subs'),
+  path('favs/', views.show_favs, name='favs'),
 ]
