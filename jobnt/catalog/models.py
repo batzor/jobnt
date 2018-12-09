e9e75b69eb3e5e8460ae73e201e582611f201053
qvntm <batzorig@kaist.ac.kr>
2018-12-09 15:18:17 +0900

    Added UserSubscription/CustomUser model.

--- jobnt/catalog/models.py
+++ jobnt/catalog/models.py
@@ -1,18 +1,39 @@
 from django.db import models
 from datetime import date
 from django.utils import timezone
+from django.core.validators import RegexValidator
 from django.contrib.auth.models import User
 import uuid
 
 # Create your models here.
+
+class CustomUser(models.Model):
+    """Base user model extension"""
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    ALL_STATUS = (
+        (0, 'Closed to offers'),
+        (1, 'Open to offers'),
+        (2, 'Actively interviewing'),
+        (3, 'Starting to look'))
+    status = models.IntegerField(default=1,choices = ALL_STATUS)
+    phone_regex = RegexValidator(regex=r'^\+?1?\d{10,15}$', message="Phone number must be entered in the format: '+123456789'. Up to 15 digits allowed.")
+    phone_number = models.CharField(validators=[phone_regex], max_length=17, blank=True)
+    occupation = models.CharField(max_length=50, null=True, blank=True)
+    is_recruiter = models.BooleanField(default=False)
 
 class Company(models.Model):
     """Model representing a company."""
     name = models.CharField(max_length=200)
     description = models.TextField(max_length = 1000, blank=True)	
     emp_number = models.IntegerField(null=True, blank=True)
+    
     def __str__(self):
         return self.name
+
+class UserSubscription(models.Model):
+    """Model representing user->company subscription"""
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
 
 class JobOffer(models.Model):
     """Model representing a job offer"""
@@ -22,10 +43,11 @@ class JobOffer(models.Model):
     salary = models.IntegerField(null=True, blank=True)
     duration = models.IntegerField(null=True, blank=True)
     description = models.TextField(max_length = 1000, blank=True)
-    date_posted = models.DateField(default=timezone.now())	
+    date_posted = models.DateField(auto_now_add=True)	
     recruiter = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     company_id = models.ForeignKey(Company, on_delete=models.CASCADE, null=True, blank=True)
     apply_link = models.URLField(blank=True)
+   
     @property
     def is_over(self):
         if self.deadline < timezone.now():
@@ -38,7 +60,7 @@ class JobOffer(models.Model):
 class Favorite(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     job = models.ForeignKey(JobOffer, on_delete=models.CASCADE)
-
+    
 class Tag(models.Model):
     name = models.CharField(max_length = 40, primary_key=True)
     
