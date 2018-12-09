a9c77278b1c208463acc8432e0e2237367d10567
Silap Aliyev <sylapaliyev@gmail.com>
2018-12-09 17:08:48 +0900

    Add favs page

--- jobnt/catalog/subs.py
+++ jobnt/catalog/subs.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 def index(request):
   args = {'subs': ['aah', 'thats', 'hot', 'thats', 'hot']}
   return render(request, 'catalog/subs.html', args)
+
+def favs(request):
+  args = {}
+  return render(request, 'catalog/favs.html', args)
--- jobnt/catalog/urls.py
+++ jobnt/catalog/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
   path('', views.index, name='index'),
   path('search/', views.search, name='search'),
   path('subs/', subs.index, name='subs'),
+  path('favs/', subs.favs, name='favs'),
 ]
