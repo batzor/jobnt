2b9b5651f6c35642c8535cb5ca8134bc8ff93375
Silap Aliyev <sylapaliyev@gmail.com>
2018-12-09 15:39:56 +0900

    Redirect homepage to /catalog instead

--- jobnt/jobnt/urls.py
+++ jobnt/jobnt/urls.py
@@ -21,6 +21,6 @@ from django.views.generic import RedirectView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('catalog/', include('catalog.urls')),
-    path('', RedirectView.as_view(url='/admin/', permanent=True)),
+    path('', RedirectView.as_view(url='/catalog/', permanent=True)),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
